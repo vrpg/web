2cbbd8289ead72997ccc58d0b93c341dd01440b7
György Bucsek <gyorgy.bucsek@gmail.com>
2018-01-02 11:48:03 +0100

    Fix uglify plugin for prod deploys

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,16 +1,13 @@
 import base from './webpack.base.config'
-import CopyWebpackPlugin from 'copy-webpack-plugin'
 import merge from 'webpack-merge'
 import webpack from 'webpack'
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
 
 export default merge(base, {
     output:{
         filename: "bundle.min.js"
     },
     plugins: [
-        /* TODO fix uglifyJS!!!
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        })*/
+        new UglifyJsPlugin()
     ]
 })
